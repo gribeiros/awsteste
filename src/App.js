6e9dbd4fe5c79a7130c9c4f3d49eb489a229f846
Gustavo R. Silva <gustavors071998@gmail.com>
2021-03-25 21:16:31 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aula 25/03/2021
         </p>
         <a
           className="App-link"
